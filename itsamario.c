41510bd0135b50428b26f779e5fda92e5e6c8755
Guillaume R <developpez@yopmail.com>
2014-03-14 15:01:12 +0100

    modified:   itsamario.c

--- itsamario.c
+++ itsamario.c
@@ -1,4 +1,8 @@
+// Compile with: $ gcc itsamario.c -I/usr/local/include -L/usr/local/lib
+// -lcs50 -o itsamario
+//
 #include <stdio.h>
+#include <cs50.h>
 
 int pyramide (int height)
 {
@@ -10,7 +14,7 @@ int pyramide (int height)
         {
            for (j=0; j < whitespace; j++)
                    printf (" ");
-           for (j=0; j < sharp; j++)
+           for (j=0; j <= sharp; j++)
                    printf ("#");
            whitespace--;
            sharp +=1;
@@ -18,11 +22,16 @@ int pyramide (int height)
         }
 }
 
+
 int main (void)
 {
     int height;
-    printf ("Enter a height please:");
-    height=getchar();
+    while ((height < 0 ) || (height > 23))
+    {
+        printf("Height:");
+        //if it's not an int GetInt() will prompt a retry...
+        height=GetInt();
+    }
     pyramide (height);
 }
 
