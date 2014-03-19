fd46ca275b6d0dcfb156094db7bb28ecc8235256
Guillaume R <developpez@yopmail.com>
2014-03-19 14:09:04 +0100

    modified:   greedy.c

--- Week1/greedy.c
+++ Week1/greedy.c
@@ -1,5 +1,4 @@
-//more complete: gcc -Wall -ggdb -I/usr/local/include -L/usr/local/lib
-//-lcs50 -lcrypt -lm
+//gcc greedy.c -lcs50 -Wall -lm
 #include<stdio.h>
 #include<math.h>
 #include<cs50.h>
@@ -8,24 +7,28 @@ int convert (int change)
 {
     int quarter,dime,nickel,penny=0;
     while (change > 0){
-        while(change >25){
+        while(change > 25){
 	    change -= 25;
 	    quarter += 1; 
+        break;
 	}
 	while (change > 10 && change < 25){
 	    change -= 10;
-            dime +=1;
+        dime +=1;
+        break;
 	}
 	while (change > 5 && change < 10){
 	    change -= 5;
 	    nickel +=1;
+        break;
 	}
 	while (change < 5 && change > 0){
-            change -=1;
-            penny+=1;
+        change -=1;
+        penny+=1;
+        break;
 	}
     }
-        printf ("Number of coins=%d",quarter+dime+nickel+penny);
+        printf ("Number of coins=%d\n",quarter+dime+nickel+penny);
 }
 
 int main(void)
@@ -33,7 +36,7 @@ int main(void)
     printf("O hai!  How much change is owed?\n");
     float sum=GetFloat();
     int change=round(sum*100);
-    printf("Change due:%d\n",change);
+    printf("Change due:%i\n",change);
     convert(change);
 }
 
