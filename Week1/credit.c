a24d623322c4547c800952025f304e38a18ce03d
Guillaume Raimondeau <raimondeau@gmail.com>
2014-03-21 19:44:02 +0100

    work in progress...

--- Week1/credit.c
+++ Week1/credit.c
@@ -4,11 +4,32 @@
 
 #define MAX_CARD 16
 
+//int measure_number(int number)
+  //int sum=0;
+  //for (i=0; i< MAX_CARD;i++)
+   //   if (number%10);{
+    //  sum+=1;
+     // number=number/10;
+    //  }
+     // else
+      //    printf("Sum:%i\n",sum);
+
+  
+   
 int main(void)
 {
     printf("Number:");
-    int s= GetLongLong();
-    int card_numbers[MAX_CARD];
+    long long int s= GetLongLong();
+    long long int card_numbers[MAX_CARD];
+
+    for (int i=0; i < MAX_CARD; i++)
+    {
+        card_numbers[i]=s%10;
+        printf("Number:%lli\n",card_numbers[i]);
+        s/=10;
+        printf("S egal:%lli\n",s);
+           
+    }
 
 
 }
