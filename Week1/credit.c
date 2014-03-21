896f01f085223d2a6f604503ccef23507aa2c68d
Guillaume R <developpez@yopmail.com>
2014-03-21 17:58:47 +0100

    to begin...

---
+++ Week1/credit.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+#include<cs50.h>
+
+#define MAX_CARD 16
+
+int main(void)
+{
+    printf("Number:");
+    int s= GetLongLong();
+    int card_numbers[MAX_CARD];
+
+
+}
