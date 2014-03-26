1cc661ed7d894e4a8fd31f7f0073b4f9c0482142
Guillaume R <developpez@yopmail.com>
2014-03-26 11:26:11 +0100

    Beginning to work on pset2

---
+++ Week2/caesar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+int main(void)
+{
+    //TEST
+    //initialiser un tableau alphabetique pour les minuscules via ascii
+    char alphabet[26];
+    //Reessayer pour les majuscules avec une double boucle for...
+    int i=0;
+    for(int a=97;a<=122;a++){
+       alphabet[i]=a;
+       printf("Prems lettre:%i %c\n",i,alphabet[i]);   
+       i+=1;
+       }
+}
