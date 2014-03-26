fc7beaf703b48393d5c41a9ee2890de1a9c43c78
Guillaume Raimondeau <raimondeau@gmail.com>
2014-03-26 20:22:48 +0100

    new file:   Makefile improvement to caesar, cipher seems to work, even with key > 26

--- Week2/caesar.c
+++ Week2/caesar.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<cs50.h>
 
 
 int main(void)
@@ -11,7 +12,24 @@ int main(void)
     int i=0;
     for(int a='a';a<='z';a++) {
        alphabet[i]=a;
-       printf("Prems lettre:%i %c\n",i,alphabet[i]);   
+       //printf("Prems lettre:%i %c\n",i,alphabet[i]);   
        i+=1;
        }
+
+    //Boucle pour trouver la position d'une lettre.
+    printf("Une lettre:");
+    char a = GetChar();
+    int found_letter=0;
+    for (int i = 0; i < 26; i++) {
+        if (alphabet[i]==a)
+            printf("trouvé %i\n",i);
+            found_letter=i;
+    }
+    printf("Key:");
+    int key=GetInt();
+    int cipher=(found_letter+key) % 26;
+    for (int i = 0; i < 26; i++) {
+        if (i == cipher)
+            printf("Encrypt: %c\n", alphabet[i]);
+    }
 }
