2a72b629fc38eee0bd0357ac09a0a7210f9c3ab8
Guillaume Raimondeau <raimondeau@gmail.com>
2014-03-26 21:16:58 +0100

    Seems to work for upper and lowercase. Got to work now to process a string

--- Week2/caesar.c
+++ Week2/caesar.c
@@ -3,20 +3,32 @@
 #include<cs50.h>
 
 
-//int main(int argc, string argv[])
-int main (void)
+int main(int argc, string argv[])
+//int main (void)
 {
     //TEST
     //initialiser un tableau alphabetique pour les minuscules via ascii
     char alphabet[26];
     //int key = atoi(argv[1]); 
     //Reessayer pour les majuscules avec une double boucle for...
+    //Be consistent: i for lowercase, y for uppercase
     int i=1;
     for(int a='a';a<='z';a++) {
        alphabet[i]=a;
-       printf("Prems lettre:%i %c\n",i,alphabet[i]);   
+       //Testing purpose
+       //printf("Prems lettre:%i %c\n",i,alphabet[i]);   
        i+=1;
        }
+
+    char AlphaBetMaj[26];
+    int y=1;
+    for(int a='A';a<='Z';a++) {
+       AlphaBetMaj[y]=a;
+       //Testing purpose
+       //printf("Prems lettre:%i %c\n",y,AlphaBetMaj[y]);   
+       y+=1;
+       }
+
 
     //Boucle pour trouver la position d'une lettre.
     // Testing purpose
@@ -29,14 +41,27 @@ int main (void)
             found_letter=i;
         }
     }
+    
+    int found_letter_maj=0;
+    for (int y = 1; y < 26; y++) {
+        if (AlphaBetMaj[y]==a){
+                printf("trouvé %i\n",y);
+                found_letter_maj=y;
+        }
+    }
+
     //Testing purpose
     printf("Key:");
     int key=GetInt();
     int cipher=(found_letter+key) % 26;
-    printf("cipher:%i\n", cipher); 
-
+    int cipher_maj=(found_letter_maj+key) % 26;
+    
+    //Need to rework on that so that when it's uppercase
+    //only the uppercase is displayed and vice-versa
     for (int i = 1; i < 26; i++) {
-        if (i == cipher)
+        if (i == cipher && found_letter !=0)
             printf("Encrypt: %c\n", alphabet[i]);
+        else if ( i == cipher_maj && found_letter_maj != 0)
+            printf("Encrypt: %c\n", AlphaBetMaj[i]);
     }
 }
