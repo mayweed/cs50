55188ab4cae029847e166b3d02e76bd5404567f7
Guillaume R <developpez@yopmail.com>
2014-03-26 14:51:46 +0100

    letters...

--- Week2/caesar.c
+++ Week2/caesar.c
@@ -6,9 +6,10 @@ int main(void)
     //TEST
     //initialiser un tableau alphabetique pour les minuscules via ascii
     char alphabet[26];
+
     //Reessayer pour les majuscules avec une double boucle for...
     int i=0;
-    for(int a=97;a<=122;a++){
+    for(int a='a';a<='z';a++) {
        alphabet[i]=a;
        printf("Prems lettre:%i %c\n",i,alphabet[i]);   
        i+=1;
