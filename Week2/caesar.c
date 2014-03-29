58b5893a762c362cce7d4ddf914eec0185e57d36
Guillaume R <developpez@yopmail.com>
2014-03-29 09:58:30 +0100

    maj is buggy...

--- Week2/caesar.c
+++ Week2/caesar.c
@@ -11,57 +11,66 @@ int main(int argc, string argv[])
     //DONT KNOW WHY!!!
     //long int key=0;
     
+    if(argc > 2 || argc < 2 ) {
+        printf("Argc:%d\n",argc);
+        printf ("You must pass one and only one argument\n");
+        return 1;
+    }
+    // Testing purpose!! Check Key Value
+    // in fact, w/o this, it does not work, it segfault???
+    //else {
+        //long int key= strtol(argv[1], NULL,10);
+      //  key= strtol(argv[1],NULL,10);
+        //printf("Key:%li\n",key); 
+        //}
+
     // Char Input
     printf("Letter:");
     char my_letter=GetChar();
 
-    if(argc > 2 || argc < 2 ) {
-            printf("Argc:%d\n",argc);
-            printf ("You must pass one and only one argument\n");
-            return 1;
-    }
-    // Testing purpose!! Check Key Value
-    // in fact, w/o this, it does not work, it segfault???
-    else {
-        long int key= strtol(argv[1], NULL,10);
-        printf("Key:%li\n",key); 
-    }
-
-    // Initialisation des alphabets
-    char alphabet[26]; 
-    int i=1;
-    for(int a='a';a<='z';a++) {
-       alphabet[i]=a;
-       //Testing purpose
-       //printf("Prems lettre:%i %c\n",i,alphabet[i]);   
-       i+=1;
-       }
-
+    // Initialisation  et recherche des alphabets
+    // Variables
     char AlphaBetMaj[26];
-    int y=1;
-    for(int a='A';a<='Z';a++) {
-       AlphaBetMaj[y]=a;
-       //Testing purpose
-       //printf("Prems lettre:%i %c\n",y,AlphaBetMaj[y]);   
-       y+=1;
-       }
-
-      
-    // Recherche séquentielle dans les alphabets
-    int found_letter=0;
-    for ( i = 1; i <= 26; i++) {
-       if (alphabet[i]==my_letter){
-           printf("trouve minuscule %i\n",i);
-           found_letter=i;
-        }
-   }
     int found_letter_maj=0;
-    for ( y = 1; y <= 26; y++) {
+    char alphabet[26]; 
+    int found_letter=0;
+
+    // Traiement
+    if (islower(my_letter)){
+        int i=1;
+
+        for(int a='a';a<='z';a++) {
+            alphabet[i]=a;
+            //Testing purpose
+            printf("Prems lettre:%i %c\n",i,alphabet[i]);   
+            i+=1;
+            }
+
+        for ( i = 1; i <= 26; i++) {
+        if (alphabet[i]==my_letter){
+            printf("trouve minuscule %i\n",i);
+            found_letter=i;
+        }
+        }
+    }
+
+    else if (isupper(my_letter)) {
+        int y=1;
+
+        for(int a='A';a<='Z';a++) {
+            AlphaBetMaj[y]=a;
+            //Testing purpose
+            printf("Prems lettre:%i %c\n",y,AlphaBetMaj[y]);   
+            y+=1;
+            }
+    
+        for ( y = 1; y <= 26; y++) {
         if (AlphaBetMaj[y]==my_letter){
             printf("trouvé majuscule %i\n",y);
             found_letter_maj=y;
         }
-   } 
+        } 
+    }
 
     // Yesterday at midnight it works?
     // Need to factor this out!!!
