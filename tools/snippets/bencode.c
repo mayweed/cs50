b5a6ea36b241db729c9db209edb349ae08efaac9
Guillaume Raimondeau <raimondeaug@localhost.localdomain>
2014-04-29 16:01:10 +0200

    mmm...

--- tools/snippets/bencode.c
+++ tools/snippets/bencode.c
@@ -15,11 +15,15 @@ if (ferror(p))
 else if (feof(p))
    printf("End of file reached successfully");
 
+//not good use a pointer or sth...Know buffer overflow??
+// Need to find the number of elts BEFORE so that I could
+// use a pointer to metainfo with a malloc: a loop with
+// fread + size as a counter?
+char metainfo[1002];
+size_t len = fread(metainfo, 1, 1000, p);
+metainfo[len] = '\0';
+printf("%s\n", metainfo);
 
-int c; // note: int, not char, required to handle EOF
-while ((c = fgetc(p)) != EOF) { 
-        putchar(c);
-       }
 
 fclose(p);
 
