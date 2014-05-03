a7029275ea79ac8a5a5414abeb3e2ffa066ffe60
Guillaume Raimondeau <raimondeau@gmail.com>
2014-05-03 10:41:42 +0200

    playing with fgets()

--- tools/snippets/bencode.c
+++ tools/snippets/bencode.c
@@ -1,31 +1,49 @@
 // Fun to test
-//
+// TODO:
+// - Rassembler les informations dans une structure
+// qui parse le torrent.
+// - Ecrire des fonctions réutilisables (via un bencode.h)
+// pour parser les réponses du tracker
 #include<stdio.h>
 #include<string.h>
+#include<ctype.h>
 
 int main(int argc, char* argv[])
 {
-char* torrent;
-torrent=argv[1];
+    char* torrent;
+    torrent=argv[1];
 
-FILE* p= fopen(torrent,"r");
+    FILE* p= fopen(torrent,"r");
 
-if (ferror(p))
-   printf("I/O error when reading");
-else if (feof(p))
-   printf("End of file reached successfully");
+    if (ferror(p))
+        printf("I/O error when reading");
+    else if (feof(p))
+        printf("End of file reached successfully");
 
-//not good use a pointer or sth...Know buffer overflow??
-// Need to find the number of elts BEFORE so that I could
-// use a pointer to metainfo with a malloc: a loop with
-// fread + size as a counter?
-char metainfo[1002];
-size_t len = fread(metainfo, 1, 1000, p);
-metainfo[len] = '\0';
-printf("%s\n", metainfo);
+    // not good use a pointer or sth...Know buffer overflow??
+    // Need to find the number of elts BEFORE so that I could
+    // use a pointer to metainfo with a malloc: a loop with
+    // fread + size as a counter?
+    char metainfo[1002];
+    //size_t len = fread(metainfo, 1, 1000, p);
+    //metainfo[len] = '\0';
+    //printf("%s\n", metainfo);
+    //char c;
+    int count=0;
 
+    fgets(metainfo,12,p);
+    for (int i=0; i < 12; i++)
+         printf("%c", metainfo[i]);
+    
+    if (isdigit(*metainfo)){
+       count= (int)metainfo;
+       printf("%i\n",count);
+    }
 
-fclose(p);
+    for (int y=0; y < count;y++)
+        printf("%c",metainfo[y]);
 
-return 0;     
+    fclose(p);
+
+    return 0;     
 }
