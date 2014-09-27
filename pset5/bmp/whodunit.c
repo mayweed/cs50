a167e11b7103fc559422580da462a7e159b72f7e
Guillaume Raimondeau <raimondeau@gmail.com>
2014-09-27 14:00:14 +0200

    iTest xxd

--- pset5/bmp/whodunit.c
+++ pset5/bmp/whodunit.c
@@ -82,8 +82,8 @@ int main(int argc, char* argv[])
             fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
 
             //Turns everything red??
-            if (triple.rgbtRed==0x00)
-                    fputc(0xFF, outptr);
+            if (triple.rgbtRed==0xFF)
+                    fputc(0x00, outptr);
 
             // write RGB triple to outfile
             fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
