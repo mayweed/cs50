7972009dc410440845476744b7461cef4d32ebeb
Guillaume Raimondeau <raimondeau@gmail.com>
2014-11-09 15:38:59 +0100

    First an array of bytes,then we will work on it

--- pset5/jpg/recover.c
+++ pset5/jpg/recover.c
@@ -8,10 +8,21 @@
  */
 
 #include<stdio.h>
+#include<stdint.h>
+
+typedef uint8_t BYTE;
 
 int main(int argc, char* argv[])
 {
     // Let's open the card
     FILE* card = fopen("card.raw", "r");
+
+    BYTE card_blocks[sizeof(card)];
+
+    while(feof(card) != 0)
+        {
+        fread(&card, sizeof(BYTE), card);
+        //fwrite(
+        }
 
 }
