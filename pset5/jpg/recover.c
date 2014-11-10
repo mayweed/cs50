26b45cd99ef4ac87dc7be92d0f88348d6d7dda8b
Guillaume Raimondeau <raimondeau@gmail.com>
2014-11-10 15:07:59 +0100

    Oki need to read a bit about pointers etc...

--- pset5/jpg/recover.c
+++ pset5/jpg/recover.c
@@ -9,6 +9,7 @@
 
 #include<stdio.h>
 #include<stdint.h>
+#include<stdlib.h>
 
 int file_length (FILE* fp)
 {
@@ -30,12 +31,15 @@ int main(int argc, char* argv[])
     int BUFSIZE=file_length(card);
 
     //Is this necessary. Can i use directly card pointer instead?
-    char card_blocks[BUFSIZE];
+    typedef uint8_t BYTE;
+    BYTE *card_blocks=malloc(BUFSIZE);
 
     for (int i=0; i < BUFSIZE; i++)
     {
-        fread(&card,sizeof(char),1,card);
+        fread(&card_blocks[i],sizeof(BYTE),1,card);
         printf("card_blocks[%i]=%x\n",i,card_blocks[i]);
     }
 
+    //should close the file and freed up mem
+    fclose(card);
 }
