f95fba376d798fcc32ca81602a0eca1b049a9e15
Guillaume Raimondeau <raimondeau@gmail.com>
2015-12-08 13:45:41 +0100

    Need to think about it more closely

--- pset5/jpg/recover.c
+++ pset5/jpg/recover.c
@@ -35,34 +35,37 @@ int main(int argc, char* argv[])
     FILE* card = fopen("card.raw", "r");
 
     // Test
-    FILE* test.raw = fopen("test.raw","w");
+    //FILE* test = fopen("test.raw","w");
 
     // Array of 512 for a block 
     int card_blocks[BLOCK];
 
     //is this really necessary?
-    //int counter=0;
+    int counter=0;
 
+    int t=0;
     // Iterate through the card
-    for(int i=0; i <= CARDSIZE;i++)
+    for(int i=counter; i <= CARDSIZE;i++)
     {
         //Iterate through each block
         for(int y=0; y <= BLOCK; y++)
         {
-        fread(&card_blocks[i],sizeof(int),1,card);
+        fread(&card_blocks[y],sizeof(int),1,card);
 
-        if(card_blocks[i]==0xff && card_blocks[i+1]==0xd8 && card_blocks[i+2]==0xff 
-                        && (card_blocks[i+3]==0xe0|| card_blocks[i+3]==0xe1))
-            fwrite(&card_blocks[i],sizeof(int),1,test.raw);
+        if(card_blocks[y]==0xff && card_blocks[y+1]==0xd8 && card_blocks[y+2]==0xff) 
+                        //&& (card_blocks[i+3]==0xe0|| card_blocks[i+3]==0xe1))
+            //fwrite(&card_blocks[i],sizeof(int),1,test.raw);
+            t+=1;
         }
     //Should add a counter here, to the next 512 block
-    //counter += 512;
+    counter += 512;
     }
 
+    printf("%d\n",t);
     //should close the file and freed up mem
     fclose(card);
 
-    fclose(test);
+    //fclose(test);
 
-    free(card_blocks);
+    //free(card_blocks);
 }
