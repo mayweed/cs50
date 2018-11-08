0f33c22bb62c0d34a36a7e0c8e456d40e9b8ee70
Guillaume Raimondeau <raimondeau@gmail.com>
2018-11-08 12:11:59 +0100

    Changes + makefile

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -7,53 +7,72 @@
  * Recovers JPEGs from a forensic image.
  */
 
-#include<cs50.h>
+//#include<cs50.h>
 #include<stdio.h>
+
 
 int main(int argc, char* argv[])
 {
+    //Usage
+    if (argc!=2){
+        fprintf(stderr,"Usage: an arg is missing ./recover your card\n");
+        return 1;
+    }
+
     // Open the card
-    FILE* card = fopen("card.raw", "r");
+    FILE *card = fopen("card.raw", "r");
+    if (card==NULL) return 2;
 
-    //char* sig="0xff 0xd8 0xff";
+     FILE *outptr=NULL;
+
+    //not int coz int == 8 bytes!! We compare bytes per bytes
+    //we want a 1-byte type
+    unsigned char buffer[512];
+
+    //https://www.tutorialspoint.com/cprogramming/c_scope_rules.htm
     int count=0;
-    int sig=0;
+    char filename[6];
 
-    while (!feof(card))
+
+
+    //Return value (ccpreference.com)
+    //Number of objects read successfully, which may be less
+    //than count if an error or end-of-file condition occurs.
+    while (fread(buffer,512,1,card)==1)
     {
-        //open a test file, which must be diff (todo)
-        FILE* fptr= fopen("test.jpg","w");
+        bool start=false;
+        //block aligned, check only the first bytes of the buffer (cf spec)
+        //the last one: F0 == 1111 0000 while e == 1110 so if you & the two you
+        //get a 'e' (as 1&0 = 0) and the 4 last bits always yields0, whatever the bits
 
-        //read a block, should i use fread here?
-        int block=fgetc(card);
-
-        if (block==0xff){
-            int second_block=fgetc(card);
-
-            if (second_block==0xd8){
-                fwrite(&block,8,1,fptr);
-                int third_block=fgetc(card);
-
-                if (third_block==0xff){
-                    fwrite(&second_block,8,1,fptr);
-                    fwrite(&third_block,8,1,fptr);
-                    eprintf("AM IN THE THIRD BLOCK\n");
-
-                    //STH is wrong here...
-                    while (block!= 0xff){
-                        int block2=fgetc(card);
-                        fwrite(&block2,8,1,fptr);
-                    }
-
-                }
-            }
-            sig+=1;
+        //check for sig in the first buffer blocks
+        if (buffer[0]==0xff && buffer[1]==0xd8 && buffer[2]==0xff && (buffer[3] & 0xf0) == 0xe0){
+            start=true;
+            count+=1;
         }
-        count+=1;
-        fclose(fptr);
+        else
+            start=false;
+
+
+        //I encountered a sig
+        if (start){
+            //one is underway
+            if (outptr != NULL){
+                fclose(outptr);
+            }
+            //count+=1;
+            sprintf(filename,"%03d.jpg",count);
+            outptr=fopen(filename,"w");
+            fwrite(&buffer,512,1,outptr);
+            fprintf(stderr,"%d\n",count);
+        }else{
+            //false start (first bytes are no good)
+            if (outptr!=NULL)
+                fwrite(&buffer,512,1,outptr);
+        }
     }
     //"you should ultimately find that the image contains 50 JPEGs."
-    //printf("%d\n",sig);
+    //eprintf("%d\n",sig);
 
     //should close the file and freed up mem
     fclose(card);
